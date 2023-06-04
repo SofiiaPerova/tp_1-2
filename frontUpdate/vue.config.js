a549562dcccfb2b31693f4a67dd3ae7d3ccf7471
Denis <skitlsskitlovich@gmail.com>
2023-06-04 16:36:51 +0300

    Production Settings 2

--- frontUpdate/vue.config.js
+++ frontUpdate/vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: { port : 5000,
-    // proxy: 'http://45.146.164.34:8080/'}
-      proxy: 'http://127.0.0.1:8000/'}
+  devServer: {
+    port: 5000,
+    proxy: 'http://45.146.164.34:8080/'
+  }
+
 })
